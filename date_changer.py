c2795b5e3fee44a383566914b0047a3d6a7187ea
Sadofyev <sadofyev@gmail.com>
2025-01-15 22:49:07 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,14 @@
+created_date = "15-01-2025"
+issue_date = "22-01-2025"
+temp_created_date = (created_date[0:5])
+temp_issue_date = (issue_date[0:5])
+print(temp_created_date)
+print(temp_issue_date)
+
+
+created_date = "created date: 15-01-2025"
+issue_date = "issue date: 22-01-2025"
+temp_created_date = (created_date[0:19])
+temp_issue_date = (issue_date[0:17])
+print(temp_created_date)
+print(temp_issue_date)
