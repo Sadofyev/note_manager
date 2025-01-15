5055fef3066c78dd5941495ff4579424cbdd9237
Sadofyev <sadofyev@gmail.com>
2025-01-15 22:10:28 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,17 @@
+print("username")
+print("title")
+print("content")
+print("status")
+print("created_date")
+print("issue_date")
+
+print("имя пользователя: username")
+print("заголовок заметки: title")
+print("описание заметки: content")
+print("статус заметки: status")
+print("дата создания заметки: created date")
+print("дата истечения заметки: issue date")
+
+# даты в формате "день-месяц-год", например "10-11-2024"
+
+
